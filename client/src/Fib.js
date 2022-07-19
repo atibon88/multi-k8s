97d2959b71652ce794562f3e3e0fe96ac9af410e
atibon88 <nickychanhk@yahoo.com.hk>
2022-07-19 22:28:10 +0800

    fix App.js blank page issue

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -5,7 +5,7 @@ class Fib extends Component {
   state = {
     seenIndexes: [],
     values: {},
-    index: ''
+    index: '',
   };
 
   componentDidMount() {
@@ -18,10 +18,10 @@ class Fib extends Component {
     this.setState({ values: values.data });
   }
 
-  async fetchIndexs(){
+  async fetchIndexes() {
     const seenIndexes = await axios.get('/api/values/all');
     this.setState({
-      seenIndexes: seenIndexes.data
+      seenIndexes: seenIndexes.data,
     });
   }
 
@@ -29,13 +29,13 @@ class Fib extends Component {
     event.preventDefault();
 
     await axios.post('/api/values', {
-      index: this.state.index
+      index: this.state.index,
     });
     this.setState({ index: '' });
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ')
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
 
   renderValues() {
@@ -44,10 +44,11 @@ class Fib extends Component {
     for (let key in this.state.values) {
       entries.push(
         <div key={key}>
-          For index {key} I calulated {this.state.values[key]}
+          For index {key} I calculated {this.state.values[key]}
         </div>
       );
     }
+
     return entries;
   }
 
@@ -58,15 +59,15 @@ class Fib extends Component {
           <label>Enter your index:</label>
           <input
             value={this.state.index}
-            onChange={event => this.setState({ index: event.target.value })}
+            onChange={(event) => this.setState({ index: event.target.value })}
           />
           <button>Submit</button>
         </form>
 
-        <h3>Index I have seen:</h3>
+        <h3>Indexes I have seen:</h3>
         {this.renderSeenIndexes()}
 
-        <h3>Calculated values:</h3>
+        <h3>Calculated Values:</h3>
         {this.renderValues()}
       </div>
     );
