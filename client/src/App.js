19d6885352de77c322e235327b184ec8188c73f6
atibon88 <nickychanhk@yahoo.com.hk>
2022-08-02 23:31:55 +0800

    EB deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Aug 2 4th
+            Learn React Aug 2 6th
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
