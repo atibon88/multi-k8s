0bdefbc9d72237d21935a70062f3e038cbac7e28
atibon88 <nickychanhk@yahoo.com.hk>
2022-08-17 19:27:08 +0800

    test GKE deploy

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React Aug 13 k8s 1st
+            Learn React Aug 16 k8s 2nd
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
